b709e28eecb66ed456ee0ee5ba43faf10e4b9ae1
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2024-10-02 18:15:03 +0300

    Fixed src directory

---
+++ src/Lexer/keywords.hpp
@@ -0,0 +1,20 @@
+#pragma once
+const char* Keywords[] = {
+    "fn",
+    "local",
+    "global",
+    "return",
+    "if",
+    "elseif",
+    "else",
+    "do",
+    "in",
+    "class",
+    "module",
+    "export",
+    "using",
+    "as",
+    "and",
+    "not",
+    "or"
+};
