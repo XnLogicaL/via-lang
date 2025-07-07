6acba541275cdfa73f4e4781f205542d012a892b
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-07-07 11:25:45 +0300

    Implement `semareg`

---
+++ src/semareg.cpp
@@ -0,0 +1,32 @@
+// This file is a part of the via Programming Language project
+// Copyright (C) 2024-2025 XnLogical - Licensed under GNU GPL v3.0
+
+#include "semareg.h"
+
+namespace via {
+
+int sema_alloc_register(const SemaRegisterState& S) {
+  for (uint16_t i = 0; i < UINT16_MAX; i++) {
+    for (uint16_t j = 0; j < 64; j++) {
+      uint64_t* reg = S.buf.data + i;
+      uint64_t mask = (1ULL << j);
+
+      if ((*reg & mask) == 0) {
+        *reg |= mask;
+        return (i * 64) + j; // absolute register address
+      }
+    }
+  }
+
+  return -1;
+}
+
+void sema_free_register(SemaRegisterState& S, int reg) {
+  uint16_t word_index = reg / 64;
+  uint16_t bit_index = reg % 64;
+
+  uint64_t mask = (1ULL << bit_index);
+  S.buf.data[word_index] |= mask; // mark bit as free
+}
+
+} // namespace via
--- src/semareg.h
+++ src/semareg.h
@@ -4,5 +4,18 @@
 #ifndef VIA_SEMAREG_H
 #define VIA_SEMAREG_H
 
+#include "common.h"
+#include "heapbuf.h"
+
+namespace via {
+
+struct SemaRegisterState {
+  HeapBuffer<uint64_t> buf{8192};
+};
+
+int sema_alloc_register(const SemaRegisterState& S);
+void sema_free_register(const SemaRegisterState& S, int reg);
+
+} // namespace via
 
 #endif
