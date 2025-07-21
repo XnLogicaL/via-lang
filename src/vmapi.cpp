4346e4a7d8100874ca03a31ef2e6e0d01e19f6ce
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-07-21 19:07:36 +0300

    Remove unused VM headers (will be replaced later)

--- src/vmapi.cpp
+++
@@ -1,6 +0,0 @@
-// This file is a part of the via Programming Language project
-// Copyright (C) 2024-2025 XnLogical - Licensed under GNU GPL v3.0
-
-#include "vmapi.h"
-
-namespace via {} // namespace via
