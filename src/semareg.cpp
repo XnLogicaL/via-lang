a393a7f7c3ede8390a3662d7e3939676e32e8210
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-07-22 11:43:35 +0300

    Fix register allocation check

--- src/semareg.cpp
+++ src/semareg.cpp
@@ -8,11 +8,11 @@ namespace via {
 int sema_alloc_register(SemaRegisterState& S) {
   for (u16 i = 0; i < UINT16_MAX; i++) {
     for (u16 j = 0; j < 64; j++) {
-      u64* reg = S.buf.data + i;
+      u64* addr = S.buf.data + i;
       u64 mask = (1ULL << j);
 
-      if ((*reg & mask) == 0) {
-        *reg |= mask;        // mark bit as occupied
+      if ((*addr & mask) == 0ULL) {
+        *addr |= mask;       // mark bit as occupied
         return (i * 64) + j; // absolute register address
       }
     }
@@ -22,8 +22,7 @@ int sema_alloc_register(SemaRegisterState& S) {
 }
 
 void sema_free_register(SemaRegisterState& S, int reg) {
-  u16 wrd = reg / 64;
-  u16 bit = reg % 64;
+  u16 wrd = reg / 64, bit = reg % 64;
   u64 mask = ~(1ULL << bit);
   S.buf.data[wrd] &= mask; // mark bit as free
 }
