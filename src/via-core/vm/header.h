5a7ab7d2b4f0f3bd2cbe5b5b5bd14e33828ce100
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-07-30 23:44:47 +0300

    Create header.h

---
+++ src/via-core/vm/header.h
@@ -0,0 +1,35 @@
+// This file is a part of the via Programming Language project
+// Copyright (C) 2024-2025 XnLogical - Licensed under GNU GPL v3.0
+
+#ifndef VIA_VM_HEADER_H_
+#define VIA_VM_HEADER_H_
+
+#include <via/config.h>
+#include "instruction.h"
+#include "lexer/location.h"
+#include "value.h"
+
+namespace via {
+
+namespace core {
+
+namespace vm {
+
+struct Header {
+  static constexpr u32 magic = 0x2E766961;  // .via
+
+  u64 flags;
+  Vec<Value> ks;        // constants
+  Vec<Instruction> is;  // Instructions
+
+  Header() = default;
+  Header(const FileBuf& buf);
+};
+
+}  // namespace vm
+
+}  // namespace core
+
+}  // namespace via
+
+#endif
