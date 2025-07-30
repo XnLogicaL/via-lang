ebffe57533178548b1db4a1b36dd294e72a27f9c
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-07-30 22:07:24 +0300

    `ptr` and `uptr` types

--- include/via/config.h
+++ include/via/config.h
@@ -111,11 +111,13 @@ using u16 = uint16_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
 using usize = size_t;
+using uptr = uintptr_t;
 
 using i8 = int8_t;
 using i16 = int16_t;
 using i32 = int32_t;
 using i64 = int64_t;
+using ptr = intptr_t;
 
 using f32 = float;
 using f64 = double;
