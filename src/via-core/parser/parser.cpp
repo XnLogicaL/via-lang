deca2c4f8b9278bb04a244fa34d61f3e592a3339
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-08-02 15:38:21 +0300

    Add support for `NodeExprLit` `PseudoValue` abstraction

--- src/via-core/parser/parser.cpp
+++ src/via-core/parser/parser.cpp
@@ -193,35 +193,38 @@ ExprNode* Parser::parse_primary() {
       lit->loc = loc;
 
       switch (lit->tok->kind) {
+        case TokenKind::NIL:
+          lit->psv.kind = PseudoValue::Nil;
+          break;
         case TokenKind::INT:
         case TokenKind::BINT:
         case TokenKind::XINT:
-          lit->kind = NodeExprLit::Int;
-          lit->u.i = stoi<vm::Value::int_type>(lit->tok->to_string())
-                         .or_else([]() -> Optional<vm::Value::int_type> {
-                           VIA_BUG("illformed int token passed to parser");
-                           return nullopt;
-                         })
-                         .value();
+          lit->psv.kind = PseudoValue::Int;
+          lit->psv.u.i = via::stoi<PseudoValue::int_type>(lit->tok->to_string())
+                             .or_else([]() -> Optional<PseudoValue::int_type> {
+                               VIA_BUG("illformed int token passed to parser");
+                               return nullopt;
+                             })
+                             .value();
           break;
         case TokenKind::FP:
-          lit->kind = NodeExprLit::Float;
-          lit->kind = NodeExprLit::Int;
-          lit->u.fp = stof<vm::Value::float_type>(lit->tok->to_string())
-                          .or_else([]() -> Optional<vm::Value::float_type> {
-                            VIA_BUG("illformed fp token passed to parser");
-                            return nullopt;
-                          })
-                          .value();
+          lit->psv.kind = PseudoValue::Float;
+          lit->psv.u.fp =
+              via::stof<PseudoValue::float_type>(lit->tok->to_string())
+                  .or_else([]() -> Optional<PseudoValue::float_type> {
+                    VIA_BUG("illformed fp token passed to parser");
+                    return nullopt;
+                  })
+                  .value();
           break;
         case TokenKind::TRUE:
         case TokenKind::FALSE:
-          lit->kind = NodeExprLit::Bool;
-          lit->u.b = lit->tok->kind == TokenKind::TRUE;
+          lit->psv.kind = PseudoValue::Bool;
+          lit->psv.u.b = lit->tok->kind == TokenKind::TRUE;
           break;
         case TokenKind::STRING:
-          lit->kind = NodeExprLit::String;
-          lit->u.str = alloc.strdup(lit->tok->to_string().c_str());
+          lit->psv.kind = PseudoValue::String;
+          lit->psv.u.str = alloc.strdup(lit->tok->to_string().c_str());
           break;
         default:
           break;
@@ -510,13 +513,12 @@ AstBuf Parser::parse() {
     }
   }
 
-  return AstBuf(nodes.data(),
-                nodes.data() + (nodes.size() * sizeof(StmtNode*)));
+  return nodes;
 }
 
-void dump_ast(const AstBuf& B) {
+void dump_ast(const AstBuf& buf) {
   usize depth = 0;
-  for (const StmtNode* stmt : B)
+  for (const StmtNode* stmt : buf)
     fmt::println("{}", stmt->get_dump(depth));
 }
 
