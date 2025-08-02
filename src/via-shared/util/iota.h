098433deb6673e74fd5670af7f2ac998ea2d627e
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-08-02 15:37:15 +0300

    Create iota.h

---
+++ src/via-shared/util/iota.h
@@ -0,0 +1,20 @@
+// This file is a part of the via Programming Language project
+// Copyright (C) 2024-2025 XnLogical - Licensed under GNU GPL v3.0
+
+#ifndef VIA_SHARED_IOTA_H_
+#define VIA_SHARED_IOTA_H_
+
+#include <via/config.h>
+
+namespace via {
+
+template <typename T>
+  requires std::is_arithmetic_v<T>
+constexpr T iota() {
+  static T old(0);
+  return old++;
+}
+
+}  // namespace via
+
+#endif
