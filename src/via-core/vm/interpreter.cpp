e6bf802e58d150ae5f17204c01ba7589f28f5d43
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-08-07 18:59:45 +0300

    Interpreter base implementation

--- src/via-core/vm/interpreter.cpp
+++ src/via-core/vm/interpreter.cpp
@@ -10,6 +10,22 @@ namespace core {
 
 namespace vm {
 
+template <typename T>
+  requires std::is_integral_v<T>
+static inline T ipow(T base, T exp) {
+  int result = 1;
+  for (;;) {
+    if (exp & 1)
+      result *= base;
+    exp >>= 1;
+    if (!exp)
+      break;
+    base *= base;
+  }
+
+  return result;
+}
+
 Stack<uptr>& Interpreter::get_stack() {
   return stack;
 }
@@ -18,21 +34,237 @@ HeapAllocator& Interpreter::get_allocator() {
   return alloc;
 }
 
-StkEntry* Interpreter::new_local(Value* val) {
-  stack.push((uptr)val);
-  return (StkEntry*)stack.top();
+ValueRef Interpreter::get_register(u16 reg) {
+  return regs.data[reg]->make_ref();
 }
 
-void Interpreter::set_local(StkEntry* id, Value* val) {
-  *id = val;
+void Interpreter::set_register(u16 reg, ValueRef val) {
+  regs.data[reg] = val.ptr;
 }
 
-Value* Interpreter::get_local(StkEntry* id) {
-  return *id;
+ValueRef Interpreter::new_local(ValueRef val) {
+  stack.push((uptr)val.ptr);
+  return get_local(stack.size() - 1);
 }
 
-ValueRef Interpreter::get_local_ref(StkEntry* id) {
-  return get_local(id)->make_ref();
+void Interpreter::set_local(usize sp, ValueRef val) {
+  *stack.at(sp) = reinterpret_cast<uptr>(val.ptr);
+}
+
+ValueRef Interpreter::get_local(usize sp) {
+  return reinterpret_cast<Value*>(stack.at(sp))->make_ref();
+}
+
+#define R(id) get_register(id)
+#define K(id) get_constant(id)
+
+void Interpreter::execute() {
+  using enum Opcode;
+
+dispatch:
+  switch (pc->op) {
+    case NOP:
+      goto dispatch;
+    case HALT:
+      goto exit;
+
+    case EXTRAARG1:
+    case EXTRAARG2:
+    case EXTRAARG3:
+      VIA_BUG("use of reserved opcode");
+
+    case IADD1:
+      R(pc->a)->u.i += R(pc->b)->u.i;
+      goto dispatch;
+    case IADD2:
+      R(pc->a)->u.i = R(pc->b)->u.i + R(pc->c)->u.i;
+      goto dispatch;
+    case IADD1K:
+      R(pc->a)->u.i += K(pc->b)->u.i;
+      goto dispatch;
+    case IADD2K:
+      R(pc->a)->u.i = R(pc->b)->u.i + K(pc->c)->u.i;
+      goto dispatch;
+    case FADD1:
+      R(pc->a)->u.fp += R(pc->b)->u.fp;
+      goto dispatch;
+    case FADD2:
+      R(pc->a)->u.fp = R(pc->b)->u.fp + R(pc->c)->u.fp;
+      goto dispatch;
+    case FADD1K:
+      R(pc->a)->u.fp += K(pc->b)->u.fp;
+      goto dispatch;
+    case FADD2K:
+      R(pc->a)->u.fp = R(pc->b)->u.fp + K(pc->c)->u.fp;
+      goto dispatch;
+
+    case ISUB1:
+      R(pc->a)->u.i -= R(pc->b)->u.i;
+      goto dispatch;
+    case ISUB2:
+      R(pc->a)->u.i = R(pc->b)->u.i - R(pc->c)->u.i;
+      goto dispatch;
+    case ISUB1K:
+      R(pc->a)->u.i -= K(pc->b)->u.i;
+      goto dispatch;
+    case ISUB2K:
+      R(pc->a)->u.i = R(pc->b)->u.i - K(pc->c)->u.i;
+      goto dispatch;
+    case ISUB2KX:
+      R(pc->a)->u.i = K(pc->b)->u.i - R(pc->c)->u.i;
+      goto dispatch;
+    case FSUB1:
+      R(pc->a)->u.fp -= R(pc->b)->u.fp;
+      goto dispatch;
+    case FSUB2:
+      R(pc->a)->u.fp = R(pc->b)->u.fp - R(pc->c)->u.fp;
+      goto dispatch;
+    case FSUB1K:
+      R(pc->a)->u.fp -= K(pc->b)->u.fp;
+      goto dispatch;
+    case FSUB2K:
+      R(pc->a)->u.fp = R(pc->b)->u.fp - K(pc->c)->u.fp;
+      goto dispatch;
+    case FSUB2KX:
+      R(pc->a)->u.fp = K(pc->b)->u.fp - R(pc->c)->u.fp;
+      goto dispatch;
+
+    case IMUL1:
+      R(pc->a)->u.i *= R(pc->b)->u.i;
+      goto dispatch;
+    case IMUL2:
+      R(pc->a)->u.i = R(pc->b)->u.i * R(pc->c)->u.i;
+      goto dispatch;
+    case IMUL1K:
+      R(pc->a)->u.i *= K(pc->b)->u.i;
+      goto dispatch;
+    case IMUL2K:
+      R(pc->a)->u.i = R(pc->b)->u.i * K(pc->c)->u.i;
+      goto dispatch;
+    case FMUL1:
+      R(pc->a)->u.fp *= R(pc->b)->u.fp;
+      goto dispatch;
+    case FMUL2:
+      R(pc->a)->u.fp = R(pc->b)->u.fp * R(pc->c)->u.fp;
+      goto dispatch;
+    case FMUL1K:
+      R(pc->a)->u.fp *= K(pc->b)->u.fp;
+      goto dispatch;
+    case FMUL2K:
+      R(pc->a)->u.fp = R(pc->b)->u.fp * K(pc->c)->u.fp;
+      goto dispatch;
+
+    case IDIV1:
+      R(pc->a)->u.i /= R(pc->b)->u.i;
+      goto dispatch;
+    case IDIV2:
+      R(pc->a)->u.i = R(pc->b)->u.i / R(pc->c)->u.i;
+      goto dispatch;
+    case IDIV1K:
+      R(pc->a)->u.i /= K(pc->b)->u.i;
+      goto dispatch;
+    case IDIV2K:
+      R(pc->a)->u.i = R(pc->b)->u.i / K(pc->c)->u.i;
+      goto dispatch;
+    case IDIV2KX:
+      R(pc->a)->u.i = K(pc->b)->u.i / R(pc->c)->u.i;
+      goto dispatch;
+    case FDIV1:
+      R(pc->a)->u.fp /= R(pc->b)->u.fp;
+      goto dispatch;
+    case FDIV2:
+      R(pc->a)->u.fp = R(pc->b)->u.fp / R(pc->c)->u.fp;
+      goto dispatch;
+    case FDIV1K:
+      R(pc->a)->u.fp /= K(pc->b)->u.fp;
+      goto dispatch;
+    case FDIV2K:
+      R(pc->a)->u.fp = R(pc->b)->u.fp / K(pc->c)->u.fp;
+      goto dispatch;
+    case FDIV2KX:
+      R(pc->a)->u.fp = K(pc->b)->u.fp / R(pc->c)->u.fp;
+      goto dispatch;
+
+    case IPOW1:
+      R(pc->a)->u.i = ipow(R(pc->a)->u.i, R(pc->b)->u.i);
+      goto dispatch;
+    case IPOW2:
+      R(pc->a)->u.i = ipow(R(pc->b)->u.i, R(pc->c)->u.i);
+      goto dispatch;
+    case IPOW1K:
+      R(pc->a)->u.i = ipow(R(pc->a)->u.i, K(pc->b)->u.i);
+      goto dispatch;
+    case IPOW2K:
+      R(pc->a)->u.i = ipow(R(pc->b)->u.i, K(pc->c)->u.i);
+      goto dispatch;
+    case IPOW2KX:
+      R(pc->a)->u.i = ipow(K(pc->b)->u.i, R(pc->c)->u.i);
+      goto dispatch;
+    case FPOW1:
+      R(pc->a)->u.fp = std::pow(R(pc->a)->u.fp, R(pc->b)->u.fp);
+      goto dispatch;
+    case FPOW2:
+      R(pc->a)->u.fp = std::pow(R(pc->b)->u.fp, R(pc->c)->u.fp);
+      goto dispatch;
+    case FPOW1K:
+      R(pc->a)->u.fp = std::pow(R(pc->a)->u.fp, K(pc->b)->u.fp);
+      goto dispatch;
+    case FPOW2K:
+      R(pc->a)->u.fp = std::pow(R(pc->b)->u.fp, K(pc->c)->u.fp);
+      goto dispatch;
+    case FPOW2KX:
+      R(pc->a)->u.fp = std::pow(K(pc->b)->u.fp, R(pc->c)->u.fp);
+      goto dispatch;
+
+    case IMOD1:
+      R(pc->a)->u.i = R(pc->a)->u.i % R(pc->b)->u.i;
+      goto dispatch;
+    case IMOD2:
+      R(pc->a)->u.i = R(pc->b)->u.i % R(pc->c)->u.i;
+      goto dispatch;
+    case IMOD1K:
+      R(pc->a)->u.i = R(pc->a)->u.i % K(pc->b)->u.i;
+      goto dispatch;
+    case IMOD2K:
+      R(pc->a)->u.i = R(pc->b)->u.i % K(pc->c)->u.i;
+      goto dispatch;
+    case IMOD2KX:
+      R(pc->a)->u.i = K(pc->b)->u.i % R(pc->c)->u.i;
+      goto dispatch;
+    case FMOD1:
+      R(pc->a)->u.fp = std::fmod(R(pc->a)->u.fp, R(pc->b)->u.fp);
+      goto dispatch;
+    case FMOD2:
+      R(pc->a)->u.fp = std::fmod(R(pc->b)->u.fp, R(pc->c)->u.fp);
+      goto dispatch;
+    case FMOD1K:
+      R(pc->a)->u.fp = std::fmod(R(pc->a)->u.fp, K(pc->b)->u.fp);
+      goto dispatch;
+    case FMOD2K:
+      R(pc->a)->u.fp = std::fmod(R(pc->b)->u.fp, K(pc->c)->u.fp);
+      goto dispatch;
+    case FMOD2KX:
+      R(pc->a)->u.fp = std::fmod(K(pc->b)->u.fp, R(pc->c)->u.fp);
+      goto dispatch;
+
+    case INEG:
+      R(pc->a)->u.i = -R(pc->b)->u.i;
+      goto dispatch;
+    case INEGK:
+      R(pc->a)->u.i = -K(pc->b)->u.i;
+      goto dispatch;
+    case FNEG:
+      R(pc->a)->u.fp = -R(pc->b)->u.fp;
+      goto dispatch;
+    case FNEGK:
+      R(pc->a)->u.fp = -K(pc->b)->u.fp;
+      goto dispatch;
+
+    default:
+      VIA_BUG("unknown opcode");
+      break;
+  }
+exit:
 }
 
 }  // namespace vm
--- src/via-core/vm/interpreter.h
+++ src/via-core/vm/interpreter.h
@@ -19,33 +19,38 @@ namespace vm {
 struct Value;
 struct ValueRef;
 
-using StkEntry = Value*;
+namespace cfg {
 
 inline constexpr usize REGISTER_COUNT = UINT16_MAX + 1;
+
+}
 
 class Interpreter {
  public:
   constexpr Interpreter(const Header* H)
-      : H(H), pc(H->is.data()), stack(&alloc), regs(REGISTER_COUNT) {
-    assert(!H->is.empty() && "illformed VM header (bytecode array empty)");
+      : H(H), pc(H->is.data()), stack(&alloc), regs(cfg::REGISTER_COUNT) {
+    assert(!H->is.empty() && "illformed header (bytecode array empty)");
   }
 
   Stack<uptr>& get_stack();
   HeapAllocator& get_allocator();
 
-  StkEntry* new_local(Value* val);
-  void set_local(StkEntry* id, Value* val);
-  Value* get_local(StkEntry* id);
-  ValueRef get_local_ref(StkEntry* id);
+  ValueRef get_constant(u16 id);
 
-  void execute_once();
+  ValueRef get_register(u16 reg);
+  void set_register(u16 reg, ValueRef val);
+
+  ValueRef new_local(ValueRef val);
+  ValueRef get_local(usize sp);
+  void set_local(usize sp, ValueRef val);
+
   void execute();
 
  private:
   const Header* H;
 
   Stack<uptr> stack;
-  Buffer<Value> regs;
+  Buffer<Value*> regs;
   HeapAllocator alloc;
 
   const uptr* fp;  // frame pointer
