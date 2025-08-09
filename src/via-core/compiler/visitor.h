70933763dcd3b3546055b7505da75502416a9a48
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-08-09 10:20:41 +0300

    Create visitor.h

---
+++ src/via-core/compiler/visitor.h
@@ -0,0 +1,88 @@
+// This file is a part of the via Programming Language project
+// Copyright (C) 2024-2025 XnLogical - Licensed under GNU GPL v3.0
+
+#ifndef VIA_CORE_VISITOR_H_
+#define VIA_CORE_VISITOR_H_
+
+#include <via/config.h>
+#include "diagnostics.h"
+#include "parser/ast.h"
+#include "sema/context.h"
+
+namespace via {
+
+namespace core {
+
+namespace compiler {
+
+class Visitor {
+ public:
+  virtual void visit(const ast::NodeExprLit& elit, u16 dst) = 0;
+  virtual void visit(const ast::NodeExprSym& esym, u16 dst) = 0;
+  virtual void visit(const ast::NodeExprUn& eun, u16 dst) = 0;
+  virtual void visit(const ast::NodeExprBin& ebin, u16 dst) = 0;
+  virtual void visit(const ast::NodeExprGroup& egrp, u16 dst) = 0;
+  virtual void visit(const ast::NodeExprCall& ecall, u16 dst) = 0;
+  virtual void visit(const ast::NodeExprSubs& esubs, u16 dst) = 0;
+  virtual void visit(const ast::NodeExprTuple& etup, u16 dst) = 0;
+  virtual void visit(const ast::NodeExprLambda& elam, u16 dst) = 0;
+
+  virtual void visit(const ast::NodeStmtVar& svar) = 0;
+  virtual void visit(const ast::NodeStmtScope& svar) = 0;
+  virtual void visit(const ast::NodeStmtIf& svar) = 0;
+  virtual void visit(const ast::NodeStmtFor& svar) = 0;
+  virtual void visit(const ast::NodeStmtForEach& svar) = 0;
+  virtual void visit(const ast::NodeStmtWhile& svar) = 0;
+  virtual void visit(const ast::NodeStmtAssign& svar) = 0;
+  virtual void visit(const ast::NodeStmtEmpty& svar) = 0;
+  virtual void visit(const ast::NodeStmtExpr& svar) = 0;
+};
+
+class TranslationVisitor : public Visitor {
+ public:
+  TranslationVisitor(Diagnostics& diag) : diag(diag) {}
+
+ protected:
+  Diagnostics& diag;
+  sema::SemaContext sema;
+};
+
+class ExprVisitor : public TranslationVisitor {
+ public:
+  using TranslationVisitor::TranslationVisitor;
+
+ public:
+  void visit(const ast::NodeExprLit& elit, u16 dst) override;
+  void visit(const ast::NodeExprSym& esym, u16 dst) override;
+  void visit(const ast::NodeExprUn& eun, u16 dst) override;
+  void visit(const ast::NodeExprBin& ebin, u16 dst) override;
+  void visit(const ast::NodeExprGroup& egrp, u16 dst) override;
+  void visit(const ast::NodeExprCall& ecall, u16 dst) override;
+  void visit(const ast::NodeExprSubs& esubs, u16 dst) override;
+  void visit(const ast::NodeExprTuple& etup, u16 dst) override;
+  void visit(const ast::NodeExprLambda& elam, u16 dst) override;
+};
+
+class StmtVisitor : public TranslationVisitor {
+ public:
+  using TranslationVisitor::TranslationVisitor;
+
+ public:
+  void visit(const ast::NodeStmtVar& svar) override;
+  void visit(const ast::NodeStmtScope& svar) override;
+  void visit(const ast::NodeStmtIf& svar) override;
+  void visit(const ast::NodeStmtFor& svar) override;
+  void visit(const ast::NodeStmtForEach& svar) override;
+  void visit(const ast::NodeStmtWhile& svar) override;
+  void visit(const ast::NodeStmtAssign& svar) override;
+  void visit(const ast::NodeStmtEmpty& svar) override;
+  void visit(const ast::NodeStmtExpr& svar) override;
+};
+
+}  // namespace compiler
+
+}  // namespace core
+
+}  // namespace via
+
+#endif
