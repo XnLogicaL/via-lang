9e0616a02ef5da63ec7b07dbb772e536bdfd3a90
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-08-09 10:22:47 +0300

    Revamp value construction pt.1

--- src/via-core/vm/value.h
+++ src/via-core/vm/value.h
@@ -17,6 +17,7 @@ namespace core {
 namespace vm {
 
 class Interpreter;
+struct PseudoValue;
 
 enum class Arith {
   Add,
@@ -50,135 +51,31 @@ struct Value {
   } u;
 
   void free();
+  Value* clone();
   ValueRef make_ref();
 
-  constexpr Optional<int_type> as_cint() const {
-    switch (kind) {
-      case Int:
-        return u.i;
-      case Float:
-        return static_cast<int_type>(u.fp);
-      case Bool:
-        return static_cast<int_type>(u.b);
-      case String: {
-        auto result = stoi<int_type>(u.str);
-        if (result.has_value())
-          return *result;
-        break;
-      }
-      default:
-        break;
-    }
+  Optional<int_type> as_cint() const;
+  Optional<float_type> as_cfloat() const;
+  bool as_cbool() const;
+  char* as_cstring() const;
 
-    return nullopt;
+  Value* as_int() const;
+  Value* as_float() const;
+  Value* as_bool() const;
+  Value* as_string() const;
+
+  template <Kind As>
+  inline Value* as() const {
+    return NULL;
   }
 
-  constexpr Optional<float_type> as_cfloat() const {
-    switch (kind) {
-      case Float:
-        return u.fp;
-      case Int:
-        return static_cast<float_type>(u.i);
-      case Bool:
-        return static_cast<float_type>(u.b);
-      case String: {
-        auto result = stof<float_type>(u.str);
-        if (result.has_value())
-          return *result;
-        break;
-      }
-      default:
-        break;
-    }
+  static Value* make(Interpreter* ctx);
+  static Value* make(Interpreter* ctx, int_type i);
+  static Value* make(Interpreter* ctx, float_type f);
+  static Value* make(Interpreter* ctx, bool b);
+  static Value* make(Interpreter* ctx, char* s);
 
-    return nullopt;
-  }
-
-  constexpr bool as_cbool() const {
-    switch (kind) {
-      case Bool:
-        return u.b;
-      case Int:
-        return static_cast<bool>(u.i);
-      case Float:
-        return static_cast<bool>(u.fp);
-      case String:
-        return true;
-      default:
-        break;
-    }
-
-    return false;
-  }
-
-  constexpr char* as_cstring() const {
-#define ALLOC_STR(str) ctx->get_allocator().strdup(str)
-
-    switch (kind) {
-      case String:
-        return ALLOC_STR(u.str);
-      case Nil:
-        return ALLOC_STR("nil");
-      case Bool:
-        return ALLOC_STR(u.b ? "true" : "false");
-      case Int:
-        return ALLOC_STR(std::to_string(u.i).c_str());
-      case Float:
-        return ALLOC_STR(std::to_string(u.fp).c_str());
-      default:
-        break;
-    }
-
-    VIA_BUG("bad kind value");
-#undef ALLOC_STR
-  }
-
-  constexpr Value as_int() const {
-    auto result = as_cint();
-    return result.has_value() ? Value(ctx, *result) : Value(ctx);
-  }
-
-  constexpr Value as_float() const {
-    auto result = as_cint();
-    return result.has_value() ? Value(ctx, *result) : Value(ctx);
-  }
-
-  constexpr Value as_bool() const {
-    return Value(ctx, as_cbool());
-  }
-
-  constexpr Value as_string() const {
-    return Value(ctx, as_cstring());
-  }
-
-  template <const Kind As>
-  inline constexpr Value as() const {
-    switch (As) {
-      case Int:
-        return as_int();
-      case Float:
-        return as_float();
-      case Bool:
-        return as_bool();
-      default:
-        break;
-    }
-
-    return Value(ctx);
-  }
-
-#define CTOR inline constexpr explicit Value
-#define TYPED_CTOR(type, ctype, id) \
-  CTOR(Interpreter* ctx, ctype id) : ctx(ctx), kind(type), u({.id = id}) {}
-
-  CTOR(Interpreter* ctx) : ctx(ctx) {}
-  TYPED_CTOR(Int, int_type, i);
-  TYPED_CTOR(Float, float_type, fp);
-  TYPED_CTOR(Bool, bool, b);
-  TYPED_CTOR(String, char*, str);
-
-#undef CTOR
-#undef TYPED_CTOR
+  static Value* from_pseudo(Interpreter* ctx, const PseudoValue& psv);
 };
 
 }  // namespace vm
